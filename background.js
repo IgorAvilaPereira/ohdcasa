5484832aa2a60d66ff61983467f1cd2905e67b3a
Igor Avila Pereira <igor.pereira@riogrande.ifrs.edu.br>
2023-11-19 21:47:34 -0300

    ok

--- background.js
+++ background.js
@@ -23,21 +23,32 @@ document.addEventListener("DOMContentLoaded", function () {
       headers: { 'Content-Type': 'application/text' }
     });
     const html = await response.text()
-    // document.querySelector("#conteudo_extensao").innerHTML = html;   
-    
+    // document.querySelector("#conteudo_extensao").innerHTML = html;    
     var parser = new DOMParser();
     var doc = parser.parseFromString(html, 'text/html');
-    var produtos = doc.getElementsByClassName("product-item__primary-image lazyload image--fade-in");   
+    var produtos = doc.getElementsByClassName("product-item__primary-image lazyload image--fade-in");    
+    var precos = doc.getElementsByClassName("product-item__price-list price-list");    
     for (let index = 0; index < produtos.length; index++) {
-       document.querySelector("#conteudo_extensao").innerHTML += produtos[index].alt + "<br><br>";
+       try {
+          var temp = precos[index].innerHTML;
+          var count = (temp.match(/R\$/g) || []).length;
+          if (count == 1) {
+            document.querySelector("#conteudo_extensao").innerHTML += produtos[index].alt + "<br>" + precos[index].innerHTML +"<br><br>";
+          } else {
+            document.querySelector("#conteudo_extensao").innerHTML += produtos[index].alt + "<br> R$ " + precos[index].innerHTML.split("R$")[1] +"<br><br>";
+          }
+       } catch (e) {
+         break;
+      }
     }
+
     // var produtos = doc.getElementsByClassName("product-item__title text--strong link");
     // var precos = doc.getElementsByClassName("price price--highlight");
     // // <button id="btn1">Buscar</button>
     // for (let index = 0; index < produtos.length; index++) {
     //   document.querySelector("#conteudo_extensao").innerHTML += produtos[index].innerText + ":" + precos[index].innerText + "<br><br>";
     // }
-    
+
     // (async () => {
     //   const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
     //   chrome.scripting.executeScript({
